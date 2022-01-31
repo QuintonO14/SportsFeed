8b5bc463666c5f2e4134fe4d18d611fb72fa6de6
QuintonO14 <quintonoconno14@gmail.com>
2022-01-31 13:46:58 -0700

    Added Next/Image source and styling fix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = withPWA({
   images : {
     domains: ['cms.nhl.bamgrid.com', 'm.files.bbci.co.uk', 'a57.foxsports.com', 'talksport.com',
   'ichef.bbci.co.uk', 'a3.espncdn.com', 'img.bleacherreport.net', 'a1.espncdn.com', 
-  'a4.espncdn.com', 'a2.espncdn.com', 'a5.espncdn.com', 'a0.espncdn.com', 'cdn.mos.cms.futurecdn.net', '']
+  'a4.espncdn.com', 'a2.espncdn.com', 'a5.espncdn.com', 'a0.espncdn.com', 'cdn.mos.cms.futurecdn.net', 
+  'a.espncdn.com']
   },
   pwa: {
     dest: "public",
